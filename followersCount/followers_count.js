c75f2f10db456138eecc56e98151e6a29f30ee1c
shah-arya <arya4shah9@gmail.com>
2025-05-01 13:22:30 -0400

    added followers count html and js file

---
+++ followersCount/followers_count.js
@@ -0,0 +1,19 @@
+let count = 0; //initialize count to 0
+
+function increaseCount(){
+    count++;
+    displayCount();
+    checkCountValue();
+}
+
+function displayCount(){
+    document.getElementById('countDisplay').innerHTML = count;
+}
+
+function checkCountValue(){
+    if(count === 10){
+        alert("Your instagram post gained 10 followers! Congratulations");
+    }else if(count === 20){
+        alert("Your instagram post gained 20 followers! Congratulations! keep it up!");
+    }
+}
