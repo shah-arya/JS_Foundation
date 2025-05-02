dde788c3d0587cb3701240da712fb84eeb16fb97
shah-arya <arya4shah9@gmail.com>
2025-05-02 16:47:24 -0400

    added total length feature

--- TextAnalysis_SpeedTest/Speed_Analysis.js
+++ TextAnalysis_SpeedTest/Speed_Analysis.js
@@ -8,12 +8,19 @@ function startTest(){
 
     // reset the results and timer
     document.getElementById("output").innerHTML = "";
+
     startTime = new Date().getTime();
 
+    const userInputEl = document.getElementById("userInput");
+    userInputEl.value = "";
+    userInputEl.readOnly = false;
+
+    userInputEl.focus();
+
     // change button text and functionality
-    var button = document.getElementById('btn');
-    button.innerHTML = "End Test";
-    button.onclick = endTest;
+    // var button = document.getElementById('btn');
+    // button.innerHTML = "End Test";
+    // button.onclick = endTest;
 }
 
 // end test function - ends the test and display the results
@@ -26,6 +33,7 @@ function endTest(){
     // calculate time elapsed and words per minute
     var timeElapsed = (endTime - startTime) / 1000; // seconds
     var userTypedText = document.getElementById("userInput").value;
+    var totalLength = userTypedText.length;
 
     // split the text using regex to count words correctly
     var typedWords = userTypedText.split(/\s+/).filter(function (word){
@@ -41,12 +49,13 @@ function endTest(){
     // display results
     var outputDiv = document.getElementById("output");
     outputDiv.innerHTML = "<h2>Typing test Results:</h2>" +
+        "<p>Total Length: " + totalLength + "</p>" +
         "<p>Words Typed: " + typedWords + "</p>" +
         "<p>Time Elapsed: " + timeElapsed.toFixed(2) + " seconds</p>" +
         "<p>Words per Minute (WPM): " + wpm + "</p>";
 
     // reset the button
-    var button = document.getElementById("btn");
-    button.innerHTML = "Start Test";
-    button.onclick = startTest;
+    // var button = document.getElementById("btn");
+    // button.innerHTML = "Start Test";
+    // button.onclick = startTest;
 }
