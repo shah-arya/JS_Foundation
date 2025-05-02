1db6c1dd7fa32cec55cd373399dd522b89a6210c
shah-arya <arya4shah9@gmail.com>
2025-05-02 10:14:52 -0400

    Added a new function findBySpecialization

--- employeeDetails/employee_details.js
+++ employeeDetails/employee_details.js
@@ -1,7 +1,7 @@
 const employees = [
-    {id: 1, name: 'ABC', age: 25, department: 'IT', salary: 43000},
-    {id: 2, name: 'DEF', age: 22, department: 'HR', salary: 45000},
-    {id: 3, name: 'John Smith', age: 30, department: 'Finance', salary: 50000}
+    {id: 1, name: 'ABC', age: 25, department: 'IT', salary: 43000, specialization: 'JavaScript'},
+    {id: 2, name: 'DEF', age: 22, department: 'HR', salary: 45000, specialization: 'Microsoft Suite'},
+    {id: 3, name: 'John Smith', age: 30, department: 'Finance', salary: 50000, specialization: 'QuickBooks'}
 ];
 
 function displayEmployees(){
@@ -9,9 +9,7 @@ function displayEmployees(){
      ${employee.name}: ${employee.age} - ${employee.department} - $${employee.salary}</p>`).join('');
 
     document.getElementById('employeesDetails').innerHTML = totalEmployees;
-
 }
-
 
 function calculateTotalSalaries(){
     const totalSalaries = employees.reduce((acc, employee)=> acc + employee.salary, 0);
@@ -30,5 +28,15 @@ function findEmployeeById(employeeID){
         document.getElementById('employeesDetails').innerHTML = `<p>${foundEmployee.id}: ${foundEmployee.name} - ${foundEmployee.department} - $${foundEmployee.salary}</p>`;
     } else{
         document.getElementById('employeesDetails').innerHTML = `<p> Employee with id: ${employeeID} does not exist in the system.</p>`;
+    }
+}
+
+function findBySpecialization(){
+    const input = prompt("Which specialization you are looking for? ");
+    const findSpecialization = employees.find(employee => employee.specialization === input);
+    if(findSpecialization){
+        document.getElementById('employeesDetails').innerHTML = `<p>${findSpecialization.id}: ${findSpecialization.name} - ${findSpecialization.department} - ${findSpecialization.specialization}</p>`;
+    } else{
+        document.getElementById('employeesDetails').innerHTML = `<p> No Employee with Specialization: ${input} exist in the system.</p>`;
     }
 }
