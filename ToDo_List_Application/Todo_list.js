5c33cee1771900a47066a1fbf0e6c8397cda0699
shah-arya <arya4shah9@gmail.com>
2025-05-04 20:34:25 -0400

    Added ClearAllTasks function

--- ToDo_List_Application/Todo_list.js
+++ ToDo_List_Application/Todo_list.js
@@ -1,8 +1,8 @@
 const taskInput = document.getElementById('taskInput');
-
 const addTaskBtn = document.getElementById('addTaskBtn');
 const taskList = document.getElementById('taskList');
 const clearCompletedBtn = document.getElementById('clearCompletedBtn');
+const clearAllBtn = document.getElementById('clearAllBtn');
 
 let tasks = [];
 
@@ -28,13 +28,11 @@ function displayTasks(){
     console.log("displaytask func");
 }
 
-
 function toggleTask(index){
     tasks[index].completed = !tasks[index].completed;
     displayTasks();
     console.log("toggletask func");
 }
-
 
 function clearCompletedTasks(){
     tasks = tasks.filter(task => !task.completed);
@@ -42,12 +40,18 @@ function clearCompletedTasks(){
     console.log("clearCompletedTasks func");
 }
 
-addTaskBtn.addEventListener("click", addTask);
-console.log("addtask click");
+function clearAllTasks(){
+    console.log("calls clearalltasks")
+    clearAllBtn.value = tasks.splice(0, tasks.length);
+    displayTasks();
+}
 
+clearAllBtn.addEventListener("click", clearAllTasks);
+
+addTaskBtn.addEventListener("click", addTask);
 
 clearCompletedBtn.addEventListener("click", clearCompletedTasks);
-console.log("clearCompletedBtn click");
 
 displayTasks();
-console.log("display task after clicks");
+
+
